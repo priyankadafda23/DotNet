c9d349e310bb47445dfb190c8d0e402332839f5e
Priyanka Dafda <priyankadafda239@gmail.com>
2026-07-21 13:34:31 +0530

    some bugs fixed.

--- Practicals/T2_11.cs
+++ Practicals/T2_11.cs
@@ -1,25 +1,38 @@
-﻿//using System;
+﻿using System;
 
-//namespace Practicals
-//{
-//    internal class T2_11
-//    {
-//        public static void Main(string[] args)
-//        {
-//            int n, sum = 0;
-//            Console.Write("Enter total number of Elements:");
-//            n = Convert.ToInt32(Console.ReadLine());
-//            int[] arr = new int[n];
-//            for(int i=0; i<n; i++)
-//            {
-//                Console.Write("Enter ELements:");
-//                arr[i] = Convert.ToInt32(Console.ReadLine());
-//            }
-//            for (int i=0; i < n; i++)
-//            {
-//                sum+= arr[i];
-//            }
-//            Console.WriteLine("Sum is {0}", sum);
-//        }
-//    }
-//}
+namespace Practicals
+{
+    internal class T2_11
+    {
+        public static void T2_11Main()
+        {
+            int n;
+            Console.Write("Enter total number of elements: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Enter element: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("Enter element to search: ");
+            int search = Convert.ToInt32(Console.ReadLine());
+
+            int found = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] == search)
+                {
+                    found = 1;
+                    break;
+                }
+            }
+
+            // Print result
+            Console.WriteLine(found);
+        }
+    }
+}
--- Practicals/T2_12.cs
+++ Practicals/T2_12.cs
@@ -1,34 +1,33 @@
-﻿//using System;
+﻿using System;
 
-//namespace Practicals
-//{
-//    internal class T2_12
-//    {
-//        public static void Main(string[] args)
-//        {
-//            int n,search;
-//            int found = 0;
-//            Console.Write("Enter total number of Elements:");
-//            n = Convert.ToInt32(Console.ReadLine());
+namespace Practicals
+{
+    internal class T2_12
+    {
+        public static void T2_12Main()
+        {
+            int n, search;
+            int found = 0;
+            Console.Write("Enter total number of Elements:");
+            n = Convert.ToInt32(Console.ReadLine());
 
-//            int[] arr = new int[n];
-//            for (int i = 0; i < n; i++)
-//            {
-//                Console.Write("Enter ELements:");
-//                arr[i] = Convert.ToInt32(Console.ReadLine());
-//            }
-//            Console.WriteLine("Enter an element to search:");
-//            search = Convert.ToInt32(Console.ReadLine());
-//            for(int i=0; i < n; i++)
-//            {
-//                if (arr[i] == search)
-//                {
-//                    found = 1;
-//                    break;
-//                }
-//                Console.WriteLine(found);
-//            }
-//        }
-
-//        }
-//    }
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Enter ELements:");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Enter an element to search:");
+            search = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] == search)
+                {
+                    found = 1;
+                    break;
+                }
+            }
+            Console.WriteLine(found);
+        }
+    }
+}
